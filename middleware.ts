d64c384f0db8ec2a4db2aebc17270dd174a78e3f
Fabricio110916 <fabricio.junior@edgeclaro.com.br>
2026-05-10 13:51:20 -0400

    Add middleware for edge runtime redirection

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  return NextResponse.redirect(
+    `https://my.koom.pp.ua${url.pathname}${url.search}`
+  );
+}
