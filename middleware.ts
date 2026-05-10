48d94397b62f40a48c7ee8ab0f68efb81202b501
Fabricio110916 <fabricio.junior@edgeclaro.com.br>
2026-05-10 14:04:27 -0400

    Refactor middleware to use NextRequest type

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export const runtime = "edge";
 
-export function middleware(request: Request) {
-  const url = new URL(request.url);
+export const config = {
+  matcher: "/:path*",
+};
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl;
 
   return NextResponse.redirect(
     `https://my.koom.pp.ua${url.pathname}${url.search}`
   );
 }
-
-export const config = {
-  matcher: "/:path*",
-};
